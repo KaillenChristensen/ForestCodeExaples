e964e62a29765edb95dd9b4ac3d0dc1619d8bde4
KaillenChristensen <132916149+KaillenChristensen@users.noreply.github.com>
2023-05-08 13:58:34 -0400

    Update FlashlightExample.cs

--- FlashlightExample.cs
+++ FlashlightExample.cs
@@ -3,34 +3,44 @@ using System.Collections;
 using Mirror;
 
 namespace Player {
-  public class FlashlightExample : MonoBehaviour {
+  public class FlashlightExample : NetworkBehaviour {
+  
+      //Variables
       public bool FlashlightActive;
       public GameObject light;
-
+      
       // Update is called once per frame
       void Update () {
+          //Only do this if on local player
+          if(!isLocalPlayer) {return;}
+      
+          //if Pressing T, Call funtion.
           if(Input.GetKeyDown(Keycode.T)
           {
-          
+            CMD_ToggleLight();
           }
       }
       
+      //tell server to run RPC_ToggleLight;
       [Command]
       void CMD_ToggleLight()
       {
         RPC_ToggleLight();
       }
       
+      //server tells all clients to toggle the players light.
+      [ClientRPC]
       void RPC_ToggleLight()
       {
           if(FlashlightActive = true)
-          {
+          { //if flashlight is on turn it off
               FlashlightActive = false;
               light.SetActive(FlashlightActive);
               Debug.Log($"Flashlight state has been set to {FlashlightActive}")
           }
           else
           {
+          //if flashlight is off, turn it on
               FlashlightActive = true;
               light.SetActive(FlashlightActive);
               Debug.Log($"Flashlight state has been set to {FlashlightActive}")
